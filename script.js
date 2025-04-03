f63315e73d8dd07d3c40c35ee29f5d75e4a18aff
deveshsingh641 <deveshsingh20666@gmail.com>
2025-04-03 18:14:50 +0530

    commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// function sayHello(){
+//     let you = prompt("What is your name ?");
+//     console.log("hello",you+"!");
+// }
+// sayHello()
+
+// let varContainingFunction = function(){
+//     let varInFunction = " i am in a function";
+//     console.log("hi there", varInFunction);
+// };
+
+// function tester(para1,para2){
+//     return para1 + " " + para2;
+// }
+// const arg1="argument1;"
+// const arg2="argument2;"
+// tester(arg1,arg2);
+
+
+// function addTwoNumbers(x, y) {
+//     console.log(x + y);
+// }
+// addTwoNumbers(5, 10);
+
+
+// function addTwoNumbers(x=2, y=3) {
+//     console.log(x + y);
+// }
+// addTwoNumbers();
+// addTwoNumbers(6, 6);
+// addTwoNumbers(10);
+
+// function addTwoNumbers(x=2,y=3) {
+//     console.log(x + y);
+// }
+// addTwoNumbers(1,2,3,4);
+
+
+let doingArrowStuff = x => console.log(x);
+
+doingArrowStuff("Hello, VS Code!");  
+
+
+
